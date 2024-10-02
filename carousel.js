fd0ca886102cbf0b5ae55763f7f3c54f4f747133
imSeptember <108691131+imSeptember@users.noreply.github.com>
2024-10-02 16:22:14 +0300

    123

--- carousel.js
+++ carousel.js
@@ -1,85 +1,85 @@
-const sliderLine = document.querySelector(".text__wrapper");
-const prevButton = document.querySelector(".left");
-const nextButton = document.querySelector(".right");
-let position = 0;
-let slideInterval;
+const sliderLine = document.querySelector('.text__wrapper')
+const prevButton = document.querySelector('.left')
+const nextButton = document.querySelector('.right')
+let position = 0
+let slideInterval
 
 const theNextSlide = () => {
-  clearInterval(slideInterval);
+  clearInterval(slideInterval)
   if (position < 80) {
-    position += 20;
+    position += 20
   } else {
-    position = 0;
+    position = 0
   }
-  sliderLine.style.transform = `translateX(-${position}%)`;
-  slideInterval = setInterval(theNextSlide, 3800);
-};
+  sliderLine.style.transform = `translateX(-${position}%)`
+  slideInterval = setInterval(theNextSlide, 3800)
+}
 
 const thePrevSlide = () => {
-  clearInterval(slideInterval);
+  clearInterval(slideInterval)
   if (position > 0) {
-    position -= 20;
+    position -= 20
   } else {
-    position = 80;
+    position = 80
   }
-  sliderLine.style.transform = `translateX(-${position}%)`;
-  slideInterval = setInterval(theNextSlide, 3800);
-};
+  sliderLine.style.transform = `translateX(-${position}%)`
+  slideInterval = setInterval(theNextSlide, 3800)
+}
 
-slideInterval = setInterval(theNextSlide, 3800);
+slideInterval = setInterval(theNextSlide, 3800)
 
-nextButton.addEventListener("click", theNextSlide);
-prevButton.addEventListener("click", thePrevSlide);
+nextButton.addEventListener('click', theNextSlide)
+prevButton.addEventListener('click', thePrevSlide)
 
 //////////////////////////////////////
 
-const firstButtonDivorce = document.getElementById("button-1-divorce");
-const secondButtonDivorce = document.getElementById("button-2-divorce");
-const thirdButtonDivorce = document.getElementById("button-3-divorce");
+const firstButtonDivorce = document.getElementById('button-1-divorce')
+const secondButtonDivorce = document.getElementById('button-2-divorce')
+const thirdButtonDivorce = document.getElementById('button-3-divorce')
 
-firstButtonDivorce.addEventListener("click", changeFirstTextDivorce);
-secondButtonDivorce.addEventListener("click", changeSecondTextDivorce);
-thirdButtonDivorce.addEventListener("click", changeThirdTextDivorce);
+firstButtonDivorce.addEventListener('click', changeFirstTextDivorce)
+secondButtonDivorce.addEventListener('click', changeSecondTextDivorce)
+thirdButtonDivorce.addEventListener('click', changeThirdTextDivorce)
 
-const firstSlideDivorce = document.getElementById("firstSlide-divorce");
-const secondSlideDivorce = document.getElementById("secondSlide-divorce");
-const thirdSlideDivorce = document.getElementById("thirdSlide-divorce");
+const firstSlideDivorce = document.getElementById('firstSlide-divorce')
+const secondSlideDivorce = document.getElementById('secondSlide-divorce')
+const thirdSlideDivorce = document.getElementById('thirdSlide-divorce')
 
-let flagDivorce = firstSlideDivorce;
+let flagDivorce = firstSlideDivorce
 
 function changeFirstTextDivorce() {
-  flagDivorce.classList.remove("visible");
-  if (!firstSlideDivorce.classList.contains("visible")) {
-    firstSlideDivorce.classList.add("visible");
-    flagDivorce = firstSlideDivorce;
+  flagDivorce.classList.remove('visible')
+  if (!firstSlideDivorce.classList.contains('visible')) {
+    firstSlideDivorce.classList.add('visible')
+    flagDivorce = firstSlideDivorce
   }
-  toggleVisibilityDivorce(firstSlideDivorce);
+  toggleVisibilityDivorce(firstSlideDivorce)
 }
 
 function changeSecondTextDivorce() {
-  flagDivorce.classList.remove("visible");
-  if (!secondSlideDivorce.classList.contains("visible")) {
-    secondSlideDivorce.classList.add("visible");
-    flagDivorce = secondSlideDivorce;
+  flagDivorce.classList.remove('visible')
+  if (!secondSlideDivorce.classList.contains('visible')) {
+    secondSlideDivorce.classList.add('visible')
+    flagDivorce = secondSlideDivorce
   }
-  toggleVisibilityDivorce(secondSlideDivorce);
+  toggleVisibilityDivorce(secondSlideDivorce)
 }
 
 function changeThirdTextDivorce() {
-  flagDivorce.classList.remove("visible");
-  if (!thirdSlideDivorce.classList.contains("visible")) {
-    thirdSlideDivorce.classList.add("visible");
-    flagDivorce = thirdSlideDivorce;
+  flagDivorce.classList.remove('visible')
+  if (!thirdSlideDivorce.classList.contains('visible')) {
+    thirdSlideDivorce.classList.add('visible')
+    flagDivorce = thirdSlideDivorce
   }
-  toggleVisibilityDivorce(thirdSlideDivorce);
+  toggleVisibilityDivorce(thirdSlideDivorce)
 }
 
 function toggleVisibilityDivorce(newFlagDivorce) {
-  flagDivorce.classList.remove("visible");
-  flagDivorce.style.opacity = "0";
-  newFlagDivorce.classList.add("visible");
+  flagDivorce.classList.remove('visible')
+  flagDivorce.style.opacity = '0'
+  newFlagDivorce.classList.add('visible')
   setTimeout(() => {
-    newFlagDivorce.style.opacity = "1";
-  }, 100);
-  flagDivorce = newFlagDivorce;
+    newFlagDivorce.style.opacity = '1'
+  }, 100)
+  flagDivorce = newFlagDivorce
 }
--- index.js
+++ index.js
@@ -1,129 +1,129 @@
 /// DYNAMIC TEXT ///
 
 const familyArr = [
-  "АДВОКАТИ З СІМЕЙНОГО ПРАВА ТА РОЗЛУЧЕНЬ У МІСТІ КИЇВ, УКРАЇНА",
-  "КОНСУЛЬТАЦІЇ ТА ВІДЕОКОНСУЛЬТАЦІЇ ДОСТУПНІ",
-  "СПРЯМОВАНІ НА ДІТЕЙ ТА ВРАХУВАННЯ ІНТЕРЕСІВ СІМ'Ї",
-];
+  'АДВОКАТИ З СІМЕЙНОГО ПРАВА ТА РОЗЛУЧЕНЬ У МІСТІ КИЇВ, УКРАЇНА',
+  'КОНСУЛЬТАЦІЇ ТА ВІДЕОКОНСУЛЬТАЦІЇ ДОСТУПНІ',
+  "СПРЯМОВАНІ НА ДІТЕЙ ТА ВРАХУВАННЯ ІНТЕРЕСІВ СІМ'Ї"
+]
 
-const familyArticle = document.querySelector(".heading__text__family");
-const discountTag = document.querySelector(".discount__family");
-let index = 0;
+const familyArticle = document.querySelector('.heading__text__family')
+const discountTag = document.querySelector('.discount__family')
+let index = 0
 
 function toggleText() {
-  familyArticle.classList.add("hidden"); // Apply the 'hidden' class to fade out the text
+  familyArticle.classList.add('hidden') // Apply the 'hidden' class to fade out the text
 
   setTimeout(() => {
     if (index !== 2) {
-      index++;
+      index++
     } else {
-      index = 0;
+      index = 0
     }
 
-    familyArticle.innerHTML = familyArr[index];
-    familyArticle.classList.remove("hidden"); // Remove the 'hidden' class to fade in the new text
-  }, 500);
+    familyArticle.innerHTML = familyArr[index]
+    familyArticle.classList.remove('hidden') // Remove the 'hidden' class to fade in the new text
+  }, 500)
 }
 
-setInterval(toggleText, 3000);
+setInterval(toggleText, 3000)
 
 /// END DYNAMIC TEXT ///
 
 /// BURGER LOGIC ///
 
-const iconMenu = document.querySelector(".menu__icon");
-const menuBody = document.querySelector(".nav__container");
-const linkTg = document.querySelector(".link__style__tg");
+const iconMenu = document.querySelector('.menu__icon')
+const menuBody = document.querySelector('.nav__container')
+const linkTg = document.querySelector('.link__style__tg')
 
-iconMenu.addEventListener("click", function (e) {
-  document.body.classList.toggle("__lock");
-  iconMenu.classList.toggle("__active");
-  menuBody.classList.toggle("__active");
-  linkTg.classList.toggle("__active");
-});
+iconMenu.addEventListener('click', function (e) {
+  document.body.classList.toggle('__lock')
+  iconMenu.classList.toggle('__active')
+  menuBody.classList.toggle('__active')
+  linkTg.classList.toggle('__active')
+})
 
 ///END BURGER LOGIC ///
 
 /// CAROUSEL ///
 
-const carouselButtons = document.querySelectorAll(".owl-dot");
+const carouselButtons = document.querySelectorAll('.owl-dot')
 
 carouselButtons.forEach((button) => {
-  button.addEventListener("click", () => {
+  button.addEventListener('click', () => {
     // Remove "active" class from all buttons
     carouselButtons.forEach((btn) => {
-      btn.classList.remove("active");
-    });
+      btn.classList.remove('active')
+    })
 
     // Add "active" class to the clicked button
-    button.classList.add("active");
-  });
-});
+    button.classList.add('active')
+  })
+})
 
-const firstButton = document.getElementById("button-1");
-const secondButton = document.getElementById("button-2");
-const thirdButton = document.getElementById("button-3");
+const firstButton = document.getElementById('button-1')
+const secondButton = document.getElementById('button-2')
+const thirdButton = document.getElementById('button-3')
 
-firstButton.addEventListener("click", changeFirstText);
-secondButton.addEventListener("click", changeSecondText);
-thirdButton.addEventListener("click", changeThirdText);
+firstButton.addEventListener('click', changeFirstText)
+secondButton.addEventListener('click', changeSecondText)
+thirdButton.addEventListener('click', changeThirdText)
 
-const firstSlide = document.getElementById("firstSlide");
-const secondSlide = document.getElementById("secondSlide");
-const thirdSlide = document.getElementById("thirdSlide");
+const firstSlide = document.getElementById('firstSlide')
+const secondSlide = document.getElementById('secondSlide')
+const thirdSlide = document.getElementById('thirdSlide')
 
-let flag = firstSlide;
+let flag = firstSlide
 
 function changeFirstText() {
-  flag.classList.remove("visible");
-  if (!firstSlide.classList.contains("visible")) {
-    firstSlide.classList.add("visible");
-    flag = firstSlide;
+  flag.classList.remove('visible')
+  if (!firstSlide.classList.contains('visible')) {
+    firstSlide.classList.add('visible')
+    flag = firstSlide
   }
-  toggleVisibility(firstSlide);
+  toggleVisibility(firstSlide)
 }
 
 function changeSecondText() {
-  flag.classList.remove("visible");
-  if (!secondSlide.classList.contains("visible")) {
-    secondSlide.classList.add("visible");
-    flag = secondSlide;
+  flag.classList.remove('visible')
+  if (!secondSlide.classList.contains('visible')) {
+    secondSlide.classList.add('visible')
+    flag = secondSlide
   }
-  toggleVisibility(secondSlide);
+  toggleVisibility(secondSlide)
 }
 
 function changeThirdText() {
-  flag.classList.remove("visible");
-  if (!thirdSlide.classList.contains("visible")) {
-    thirdSlide.classList.add("visible");
-    flag = thirdSlide;
+  flag.classList.remove('visible')
+  if (!thirdSlide.classList.contains('visible')) {
+    thirdSlide.classList.add('visible')
+    flag = thirdSlide
   }
-  toggleVisibility(thirdSlide);
+  toggleVisibility(thirdSlide)
 }
 
 function toggleVisibility(newFlag) {
-  flag.classList.remove("visible");
-  flag.style.opacity = "0";
-  newFlag.classList.add("visible");
+  flag.classList.remove('visible')
+  flag.style.opacity = '0'
+  newFlag.classList.add('visible')
   setTimeout(() => {
-    newFlag.style.opacity = "1";
-  }, 100);
-  flag = newFlag;
+    newFlag.style.opacity = '1'
+  }, 100)
+  flag = newFlag
 }
 
 ///END CAROUSEL ///
 
 /// HIDE TEXT ///
 
-const buttonHide = document.querySelector(".frm_btn");
-const manualHide = document.querySelector(".manual__hide");
+const buttonHide = document.querySelector('.frm_btn')
+const manualHide = document.querySelector('.manual__hide')
 
-buttonHide.addEventListener("click", function hideText() {
-  buttonHide.style.display = "none";
-  manualHide.style.display = "block";
+buttonHide.addEventListener('click', function hideText() {
+  buttonHide.style.display = 'none'
+  manualHide.style.display = 'block'
   setTimeout(() => {
-    manualHide.style.opacity = "1";
-  }, 100);
-});
+    manualHide.style.opacity = '1'
+  }, 100)
+})
 
 ///END HIDE TEXT  ///
