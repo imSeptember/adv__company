924f9c347eaadd694c16052d72f7b5d73cb5fde1
imSeptember <108691131+imSeptember@users.noreply.github.com>
2024-11-08 01:33:02 +0200

    s

--- FONTS/Icon-font-7-stroke-PIXEDEN-v-1-2-0/Icon-font-7-stroke-PIXEDEN-v-1.2.0/assets/refs/main.js
+++ FONTS/Icon-font-7-stroke-PIXEDEN-v-1-2-0/Icon-font-7-stroke-PIXEDEN-v-1.2.0/assets/refs/main.js
@@ -1,123 +1,120 @@
+$(document).ready(function () {
+  $('.selectpicker').selectpicker()
 
-$(document).ready(function(){
+  let val = $('.selectpicker option:selected').val()
+  $('.font-icon-detail span:first-child').css('font-size', val + 'px')
+  $('#content')
+    .removeClass()
+    .addClass('size' + val)
 
-    $('.selectpicker').selectpicker();    
+  $('.selectpicker').change(function () {
+    val = $('.selectpicker option:selected').val()
+    $('.font-icon-detail span:first-child').css('font-size', val + 'px')
+    $('#content')
+      .removeClass()
+      .addClass('size' + val)
+  })
 
-    val = $('.selectpicker option:selected').val();
-    $('.font-icon-detail span:first-child').css( "font-size", val + "px" );
-    $('#content').removeClass().addClass("size"+val);
+  $('#chkbx-1').change(function () {
+    if ($(this).is(':checked')) {
+      $('.font-icon-code').addClass('show')
+      return
+    }
+    $('.font-icon-code').removeClass('show')
+  })
 
-    $('.selectpicker').change(function(){
-        val = $('.selectpicker option:selected').val();
-        $('.font-icon-detail span:first-child').css( "font-size", val + "px" );
-        $('#content').removeClass().addClass("size"+val);
-    });
+  $('#s1').change(function () {
+    if ($(this).is(':checked')) {
+      $('body').addClass('dark')
+      return
+    }
+    $('body').removeClass('dark')
+  })
 
-    $('#chkbx-1').change(function() {
-       if($(this).is(":checked")) {
-            $('.font-icon-code').addClass('show');
-          return;
-       }
-        $('.font-icon-code').removeClass('show');
-    });
+  $('#nav').affix({
+    offset: { top: $('#nav').offset().top }
+  })
 
-    $('#s1').change(function() {
-       if($(this).is(":checked")) {
-          $('body').addClass("dark");
-          return;
-       }
-      $('body').removeClass("dark");
-    });
+  $('#nav').on('affix.bs.affix', function () {
+    $('#font_options_bar').height($('#nav').height())
+  })
 
-    $('#nav').affix({
-        offset: { top: $('#nav').offset().top },
-    });
+  $('#nav').on('affixed-top.bs.affix', function () {
+    $('#font_options_bar').height('auto')
+  })
 
-    $('#nav').on('affix.bs.affix', function () {
-        $('#font_options_bar').height($("#nav").height());
-    });
+  $('.code-value, .unicode, .unicode-text, .font-icon-name').click(function () {
+    $(this).select()
+  })
 
-    $('#nav').on('affixed-top.bs.affix', function () {
-        $('#font_options_bar').height('auto');
-    });
+  $('.font-icon-detail').on('click', function () {
+    $(this).find('.font-icon-name').select()
+  })
 
-    $(".code-value, .unicode, .unicode-text, .font-icon-name").click(function(){
-      $(this).select();
-    });
-
-    $('.font-icon-detail').on("click", function(){
-      $(this).find('.font-icon-name').select();
-    });
-
-
-    searchShow.onchange = function(){
-      if(this.checked) {
-        setTimeout(function(){
-          search.focus()  
-        },50)
+  // Проверяем наличие элементов перед использованием
+  if (search && searchShow && emptySearch) {
+    searchShow.onchange = function () {
+      if (this.checked) {
+        setTimeout(function () {
+          search.focus()
+        }, 50)
       }
     }
 
-    search.onchange = function(){
-      doSearch(this);
+    search.onchange = function () {
+      doSearch(this)
     }
-    search.onkeyup = function(e){
+
+    search.onkeyup = function (e) {
       if (e.keyCode == 27) {
-        this.value = "";
-        searchShow.checked = false;
-      } else {
+        // Escape key
+        this.value = ''
+        searchShow.checked = false
       }
-      doSearch(this);
+      doSearch(this)
     }
-
-
+  }
 }) // Ready
 
+// Обработка события прокрутки с passive: true
+$(window).on('scroll', { passive: true }, function () {
+  var opacityValue = 1 - $(window).scrollTop() / 315
+  return $('#showcase hgroup').css('opacity', opacityValue)
+})
 
-$(window).scroll(function() {
-  var opacityValue;
-  opacityValue = 1 - $(window).scrollTop() / 315;
-  return $("#showcase hgroup").css("opacity", opacityValue);
-});
+var search = document.querySelector('#search')
+var searchShow = document.querySelector('#search-show')
+var emptySearch = document.querySelector('#search-empty')
 
-var search = document.querySelector('#search');
-var searchShow = document.querySelector('#search-show');
-var emptySearch = document.querySelector('#search-empty');
+function doSearch(filter) {
+  var filterValue = filter.value
+  var allIcons = document.getElementById('all-icons')
+  if (!allIcons) return // Проверка наличия элемента 'all-icons'
 
+  var all = allIcons.querySelectorAll('.font-icon-list')
+  var contains = allIcons.querySelectorAll(
+    '.font-icon-name[value*="' + filterValue + '"]'
+  )
 
-function doSearch(filter){
-    var filter = filter.value;
-    var allIcons = document.getElementById('all-icons');
-    var all = allIcons.querySelectorAll('.font-icon-list');
-    var contains = allIcons.querySelectorAll('.font-icon-name[value*="' + filter + '"]');
-
-    if(filter != "") {
-      for (var i = 0; i < all.length; i++) {
-        all[i].style.display = "none";
-      };
-
-      for (var i = 0; i < contains.length; i++) {
-        contains[i].parentElement.parentElement.style.display = "block";
-      };
-
-      if(contains.length == 0){
-        emptySearch.style.display = "block";
-        emptySearch.querySelector('strong').innerHTML = search.value;
-      } else {
-        emptySearch.style.display = "none";
-      }
-    } else {
-      for (var i = 0; i < all.length; i++) {
-        all[i].style.display = "block";
-      };
-      emptySearch.style.display = "none";
+  if (filterValue !== '') {
+    for (var i = 0; i < all.length; i++) {
+      all[i].style.display = 'none'
     }
 
+    for (var i = 0; i < contains.length; i++) {
+      contains[i].parentElement.parentElement.style.display = 'block'
+    }
 
+    if (contains.length === 0) {
+      emptySearch.style.display = 'block'
+      emptySearch.querySelector('strong').innerHTML = search.value
+    } else {
+      emptySearch.style.display = 'none'
+    }
+  } else {
+    for (var i = 0; i < all.length; i++) {
+      all[i].style.display = 'block'
+    }
+    emptySearch.style.display = 'none'
+  }
 }
-
-
-
-
-
-
